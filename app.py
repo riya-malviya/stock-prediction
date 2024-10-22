344a37369e699e9f349970214a44500ebea09b67
riya-malviya <riyamalviya02@gmail.com>
2024-10-22 20:11:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,6 @@ else:
     
       # Predict forecast with Prophet.   
       df_train = data[['Date','Close']]
-      df_train['Close'] = df_train['Close'].values.flatten()  # Ensuring it's 1D
       df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
     
       m = Prophet()
